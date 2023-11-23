4cad42030d45eb7036d3822e12cf21e078b297f9
itsfelipe-dev <aforjuela11@misena.edu.co>
2023-11-22 20:57:51 -0500

    FIX: remove redundant code

--- app/app.py
+++ app/app.py
@@ -4,9 +4,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-GCP_PROJECT_ID = os.getenv('GCP_PROJECT_ID')
-SERVICE_ACCOUNT_FILE = os.getenv('SERVICE_ACCOUNT_FILE')
-STORAGE_BUCKET_NAME = os.getenv('STORAGE_BUCKET_NAME')
 
 HOST_DB = os.getenv('HOST_DB')
 USER_DB = os.getenv('USER_DB')
